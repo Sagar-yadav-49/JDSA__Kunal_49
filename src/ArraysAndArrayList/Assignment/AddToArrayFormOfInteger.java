39cc9eb39e70f7808c4f04b80e8828c6d8e39b59
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-09 13:20:00 +0530

    Update code of AddToArrayFormOfInteger Leetcode

--- src/ArraysAndArrayList/Assignment/AddToArrayFormOfInteger.java
+++ src/ArraysAndArrayList/Assignment/AddToArrayFormOfInteger.java
@@ -2,28 +2,28 @@ package ArraysAndArrayList.Assignment;
 // wrong
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class AddToArrayFormOfInteger {
     public static void main(String[] args) {
-        int[] num={1,2,0,0};
-        int k=34;
+        int[] num={2,1,5};
+        int k=806;
         for(int ele:addToArrayForm(num,k)){
             System.out.print(ele);
         }
+
     }
     public static List<Integer> addToArrayForm(int[] num, int k) {
         List<Integer> list=new ArrayList<>();
-        int sum=0;
-        for (int i = num.length-1; i >0 ; i--) {
-            sum=num[i]+(k%10);
-            list.add(0,sum%10);
-            k=sum/10;
+        for (int i = num.length-1; i >=0 || k>0; i--) {
+            if(i>=0){
+                k+=num[i];
+            }
+            list.add(k%10);
+            k/=10;
         }
-        while (k > 0) {
-            list.add(0, k % 10);
-            k /= 10;
-        }
+        Collections.reverse(list);
         return list;
     }
 }
---
+++ src/BinarySearchAlgorithm/InterviewQuestions/FindInMountainArray.java
@@ -0,0 +1,8 @@
+package BinarySearchAlgorithm.InterviewQuestions;
+
+public class FindInMountainArray {
+    public static void main(String[] args) {
+
+    }
+    
+}
