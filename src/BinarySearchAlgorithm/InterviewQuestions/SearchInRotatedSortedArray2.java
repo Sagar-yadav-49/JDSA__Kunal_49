dc810807dfa10828c82e7e6bf813288d79c4cc31
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-12 19:26:40 +0530

    Leetcode search in rotated sorted array if duplicates include

---
+++ src/BinarySearchAlgorithm/InterviewQuestions/SearchInRotatedSortedArray2.java
@@ -0,0 +1,13 @@
+package BinarySearchAlgorithm.InterviewQuestions;
+
+public class SearchInRotatedSortedArray2 {
+    public static void main(String[] args) {
+        int[] nums={7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int target=3;
+        System.out.println(search(nums,target));
+    }
+    public static boolean search(int[] nums, int target) {
+
+        return false;
+    }
+}
