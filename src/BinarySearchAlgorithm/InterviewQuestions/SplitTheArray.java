215f62b0b139a25c6d36abaa82b3c218180a6f81
Sagar Yadav <sagaryadav6849@gmail.com>
2026-07-15 22:38:49 +0530

    LeetCode Split the array

---
+++ src/BinarySearchAlgorithm/InterviewQuestions/SplitTheArray.java
@@ -0,0 +1,41 @@
+package BinarySearchAlgorithm.InterviewQuestions;
+
+import java.util.Arrays;
+
+public class SplitTheArray {
+    public static void main(String[] args) {
+        int[] nums={7,2,5,10,8};
+        int k=2;
+        System.out.println(splitArray(nums,k));
+    }
+    public static int splitArray(int[] a, int k) {
+        int low = Arrays.stream(a).max().getAsInt();
+        int high = Arrays.stream(a).sum();
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(a, mid);
+
+            if (partitions > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    public static int countPartitions(int[] a, int maxSum) {
+        int partitions = 1;
+        long subarraySum = 0;
+
+        for (int num : a) {
+            if (subarraySum + num <= maxSum) {
+                subarraySum += num;
+            } else {
+                partitions++;
+                subarraySum = num;
+            }
+        }
+        return partitions;
+    }
+}
